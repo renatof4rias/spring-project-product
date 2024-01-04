7dbd7deb77ebccb1475f9df603bdfe94585d7d97
renatof4rias <renatof4rias@gmail.com>
2024-01-04 20:48:17 -0300

    Entity Product created

--- src/main/java/com/softelse/DemoApplication.java
+++ src/main/java/com/softelse/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.softelse.demo;
+package com.softelse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/softelse/entities/Product.java
@@ -0,0 +1,43 @@
+package com.softelse.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_products")
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private UUID id;
+	private String name;
+	private BigDecimal price;
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+}
