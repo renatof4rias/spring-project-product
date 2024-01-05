7b50c6d804a97a940a2b8bd9ce522165a364efc3
renatof4rias <renatof4rias@gmail.com>
2024-01-04 21:48:26 -0300

    Get All Product

--- src/main/java/com/softelse/resources/ProductResource.java
+++ src/main/java/com/softelse/resources/ProductResource.java
@@ -1,9 +1,15 @@
 package com.softelse.resources;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +32,11 @@ public class ProductResource {
 		BeanUtils.copyProperties(productRecordDto, product);
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
 	}
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<Product>> getAllProducts(){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+	}
+	
 	
 }
