097e186eafd3367fccd8d735e09de436cddb6651
renatof4rias <renatof4rias@gmail.com>
2024-01-04 22:32:41 -0300

    Delete Product

--- src/main/java/com/softelse/resources/ProductResource.java
+++ src/main/java/com/softelse/resources/ProductResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +44,7 @@ public class ProductResource {
 	public ResponseEntity<Object> getOneProduct(@PathVariable(value="id")UUID id){
 		Optional<Product> product = repository.findById(id);
 		if(product.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(product.get());
 	}
@@ -52,12 +53,24 @@ public class ProductResource {
 	public ResponseEntity<Object> updateProduct(@PathVariable(value="id") UUID id, @RequestBody @Valid ProductRecordDto productRecordDto){
 		Optional<Product> product = repository.findById(id);
 		if(product.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
 		}
 		var product1 = product.get();
 		BeanUtils.copyProperties(productRecordDto, product1);
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(product1));
 	}
+	
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") UUID id){
+		Optional<Product> product = repository.findById(id);
+		if(product.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+		}
+		repository.delete(product.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+	}
+	
+	
 	
 	
 }
