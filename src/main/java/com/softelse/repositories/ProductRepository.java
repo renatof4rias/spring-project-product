057b760635b8e8d8eb542710fd0236b65788a74c
renatof4rias <renatof4rias@gmail.com>
2024-01-07 13:28:09 -0300

    JPA

--- src/main/java/com/softelse/repositories/ProductRepository.java
+++ src/main/java/com/softelse/repositories/ProductRepository.java
@@ -3,7 +3,6 @@ package com.softelse.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.softelse.entities.Product;
 
